ffdfa97393f1f38abe1494f327ff7ddcc8b7aeff
Ilija1076 <ij20201076@student.fon.bg.ac.rs>
2023-09-16 18:12:45 +0200

    Navigacioni meni, adminDashboard, proizvodi

---
+++ my-react-project/src/AdminPage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import $ from "jquery";
+import "datatables.net-dt";
+import "datatables.net-responsive-dt";
+const AdminPage = ({proizvodi,orders}) => {
+
+
+
+  useEffect(() => {
+    if (orders.length) {
+      $("#orders-table").DataTable();
+    }
+  }, [orders]);
+
+
+
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <table id="orders-table" className="display responsive nowrap">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>User</th>
+            <th>Created At</th> 
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order.id}>
+              <td>{order.id}</td>
+              <td>{order.status}</td>
+              <td>{order.user.name}</td>
+            
+              
+              <td>{new Date(order.created_at).toLocaleString()}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ my-react-project/src/Navbar.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Navbar = ({ user, onLogout }) => {
+    let navigate=useNavigate();
+    const handleLogout = () => {
+        axios.post('http://127.0.0.1:8000/api/logout', null, {
+            headers: {
+                Authorization: `Bearer ${window.sessionStorage.getItem("auth_token")}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            onLogout();
+            navigate("/");
+        })
+        .catch(error => console.error(error));
+    };
+    
+
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">Web prodavnica
+        
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto">
+
+            {user && user.role=='admin' && (
+                <><li className="nav-item">
+                    <NavLink to="/admin" className="nav-link" activeClassName="active">AdminDashboard</NavLink>
+                    
+
+                </li>
+                <li className="nav-item">
+                <   NavLink to="/admin/products" className="nav-link" activeClassName="active">Proizvodi</NavLink>            
+
+                </li>
+                <li className="nav-item">
+                <   NavLink to="/admin/products/add" className="nav-link" activeClassName="active">Dodaj</NavLink>            
+
+                </li>
+                <li className="nav-item">
+                <   NavLink to="/admin/stats" className="nav-link" activeClassName="active">Statistike</NavLink>            
+
+                </li>
+                </>
+
+
+            )}
+
+            { user &&  user.role!='admin' &&
+
+                (<><li className="nav-item">
+                <NavLink to="/" className="nav-link" activeClassName="active" end>Home</NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink to="/proizvodi" className="nav-link" activeClassName="active">Proizvodi</NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink to="/korpa" className="nav-link" activeClassName="active">Korpa</NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink to="/preporuke" className="nav-link" activeClassName="active" end>Nase preporuke</NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink to="/recepti" className="nav-link" activeClassName="active">Recepti</NavLink>
+                </li>
+                
+                
+                </>)
+
+            }
+          </ul>
+          <ul className="navbar-nav">
+            {user ? (
+              <>
+
+                <li className="nav-item">
+                  <span className="nav-link">Dobrodošli, {user.username}</span>
+                </li>
+                <li className="nav-item">
+                  <button className="btn btn-outline-primary" onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link" activeClassName="active">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link" activeClassName="active">Register</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
